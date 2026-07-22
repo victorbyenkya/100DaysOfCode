f54bcfdea65879f35ec81557ee254c8cafcd3ffb
victorbyenkya <byenkyavictor@gmail.com>
2026-07-22 23:47:44 +0300

    Update python fundamentals code

---
+++ 01_python_fundamentals/02_programming_terminology.py
@@ -0,0 +1,40 @@
+# %%
+#Statement
+#below it's a print and assignment statement within a conditional statement.
+
+name = 'Ann'
+if name == 'Ann':
+    print('Hi!')
+    number = 1
+# %%
+#Block
+age = int(input('What is your age? '))
+#define age before using it!
+if age > 17:
+    #beginning of a conditional block
+    print('You are of age!')
+    age = age + 1
+    print('You are now a year older.....')
+
+print('This here belongs to another block..')
+
+
+# %%
+#Expressions
+#Because all expressions have types they can be assigned to variables
+
+#The variable x is assigned to the value of the expression 1 + 2
+
+x = 1 + 2
+
+3 * x + x ** 2
+
+
+
+
+# %%
+#Finding out the data type
+
+print(type('Anna'))
+print(type(100))
+# %%
